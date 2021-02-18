315cc156323219e2664a95507392279135a10789
zhongzhiyuan <zhongzhiyuan5@gmail.com>
2021-02-18 22:12:47 +0800

    优化生成周线

--- app/Console/Commands/GenerateStockWeek.php
+++ app/Console/Commands/GenerateStockWeek.php
@@ -2,6 +2,7 @@
 
 namespace App\Console\Commands;
 
+use App\Http\Controllers\UpdateController;
 use App\Models\Stock;
 use App\Models\StockDaily;
 use App\Models\StockWeek;
@@ -39,106 +40,12 @@ class GenerateStockWeek extends Command
      *
      * @return mixed
      */
-    public function handle()
+    public function handle(UpdateController $updateController)
     {
         //
         $date = $this->option("date");
-       $allStocks = Stock::all(["id"]);
-       $isWeek = $this->option("week");
-       foreach ($allStocks as $index => $stock) {
-           $this->info("dealing {$index}");
-           $stockDaily = null;
-           if ($isWeek == false) {
-               if ($date == null) {
-                   $stockDaily = StockDaily::where('stock_id', $stock->id)->orderBy("trade_date")->get();
-               } else {
-                   $stockDaily = StockDaily::where('stock_id', $stock->id)
-                       ->where('trade_date', '>=', $date)
-                       ->orderBy("trade_date")->get();
-               }
-           } else {
-               list($mondayStr, $fridayStr) = $this->calcMondayAndFriday(now()->format("Ymd"));
-               $stockDaily = StockDaily::where('stock_id', $stock->id)
-                   ->where('trade_date', '>=', $mondayStr)
-                   ->where('trade_date', '<=', $fridayStr)
-                   ->orderBy("trade_date")->get();
-           }
-           if (count($stockDaily) == 0) {
-              continue;
-           }
-           $date = $stockDaily[0]->trade_date;
-           list($mondayStr, $fridayStr) = $this->calcMondayAndFriday($date);
-           $insertStockWeekData = [];
-           $weekData = [];
-           $lastClose = 0;
-           foreach ($stockDaily as $daily) {
-               if (intval($daily->trade_date) >= $mondayStr && intval($daily->trade_date) <= $fridayStr) { //在本周
-                   $weekData[] = $daily;
-               } else {
-                   if (empty($weekData) == false) {
-                       $insertStockWeekData[] = $this->dealWeekData($weekData,$lastClose);
-                   }
-                   list($mondayStr, $fridayStr) = $this->calcMondayAndFriday($daily->trade_date);
-                   $weekData = [];
-                   $weekData[] = $daily;
-               }
-           }
-
-           if (empty($weekData) == false) {
-               $insertStockWeekData[] = $this->dealWeekData($weekData, $lastClose);
-           }
-           if ($isWeek == false) {
-               if ($date) {
-                   foreach ($insertStockWeekData as $data) {
-                       $exists = StockWeek::where('stock_id', $data["stock_id"])->where('trade_date', $data["trade_date"])->exists();
-                       if ($exists == false) {
-                           StockWeek::create($data);
-                       } else {
-                           StockWeek::where('stock_id', $data["stock_id"])->where('trade_date', $data["trade_date"])->update($data);
-                       }
-                   }
-               } else {
-                   StockWeek::insert($insertStockWeekData);
-               }
-           } else {
-               foreach ($insertStockWeekData as $data) {
-                   $exists = StockWeek::where('stock_id', $data["stock_id"])->where('trade_date', $data["trade_date"])->exists();
-                   if($exists) {
-                       StockWeek::where('stock_id', $data["stock_id"])->where('trade_date', $data["trade_date"])->update($data);
-                   } else {
-                       StockWeek::create($data);
-                   }
-               }
-           }
-       }
+        $isWeek = $this->option("week");
+        $updateController->generatorWeekStock($date, $isWeek);
     }
 
-    function dealWeekData($weekData, &$lastClose) {
-        $weekDataCollect = Collect($weekData);
-        $change = $weekDataCollect->last()->close - $weekData[0]->open;
-        $pct_chg = $change / $weekData[0]->open;
-        $result = [
-            "stock_id" => $weekData[0]->stock_id,
-            "trade_date" => $weekDataCollect->last()->trade_date,
-            "open" => $weekData[0]->open,
-            "high" => $weekDataCollect->max("high"),
-            "low" => $weekDataCollect->max("low"),
-            "close" => $weekDataCollect->last()->close,
-            "pre_close" => $lastClose,
-            "change" => $weekDataCollect->last()->close - $weekData[0]->open,
-            "pct_chg" => $pct_chg,
-            "vol" => $weekDataCollect->sum('vol'),
-            "amount" => $weekDataCollect->sum('amount'),
-            "fq_factor" => $weekDataCollect->last()->fq_factor
-        ];
-        $lastClose = $weekDataCollect->last()->close;
-        return $result;
-    }
-    public function calcMondayAndFriday($dateStr) {
-
-        $d = Carbon::createFromFormat("Ymd", $dateStr);
-        $monday = $d->copy()->subDays($d->dayOfWeekIso - 1);
-        $friday = $d->copy()->addDays(5 - $d->dayOfWeekIso);
-        return [$monday->format("Ymd"), $friday->format("Ymd")];
-    }
 }
--- app/Http/Controllers/UpdateController.php
+++ app/Http/Controllers/UpdateController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Client\TushareClient;
+use App\Models\Stock;
+use App\Models\StockDaily;
+use App\Models\StockWeek;
 use App\Models\TradeDate;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Log;
 
 class UpdateController extends Controller
 {
@@ -29,5 +34,111 @@ class UpdateController extends Controller
         }
         TradeDate::insert($insert_data);
 
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function generatorWeekStock($date, $isWeek)
+    {
+        //
+        $allStocks = Stock::all(["id"]);
+        foreach ($allStocks as $index => $stock) {
+            Log::info("dealing {$index}");
+            $stockDaily = null;
+            if ($isWeek == false) {
+                if ($date == null) {
+                    $stockDaily = StockDaily::where('stock_id', $stock->id)->orderBy("trade_date")->get();
+                } else {
+                    $stockDaily = StockDaily::where('stock_id', $stock->id)
+                        ->where('trade_date', '>=', $date)
+                        ->orderBy("trade_date")->get();
+                }
+            } else {
+                list($mondayStr, $fridayStr) = $this->calcMondayAndFriday(now()->format("Ymd"));
+                $stockDaily = StockDaily::where('stock_id', $stock->id)
+                    ->where('trade_date', '>=', $mondayStr)
+                    ->where('trade_date', '<=', $fridayStr)
+                    ->orderBy("trade_date")->get();
+            }
+            if (count($stockDaily) == 0) {
+                continue;
+            }
+            $date = $stockDaily[0]->trade_date;
+            list($mondayStr, $fridayStr) = $this->calcMondayAndFriday($date);
+            $insertStockWeekData = [];
+            $weekData = [];
+            $lastClose = 0;
+            foreach ($stockDaily as $daily) {
+                if (intval($daily->trade_date) >= $mondayStr && intval($daily->trade_date) <= $fridayStr) { //在本周
+                    $weekData[] = $daily;
+                } else {
+                    if (empty($weekData) == false) {
+                        $insertStockWeekData[] = $this->dealWeekData($weekData,$lastClose);
+                    }
+                    list($mondayStr, $fridayStr) = $this->calcMondayAndFriday($daily->trade_date);
+                    $weekData = [];
+                    $weekData[] = $daily;
+                }
+            }
+
+            if (empty($weekData) == false) {
+                $insertStockWeekData[] = $this->dealWeekData($weekData, $lastClose);
+            }
+            if ($isWeek == false) {
+                if ($date) {
+                    foreach ($insertStockWeekData as $data) {
+                        $exists = StockWeek::where('stock_id', $data["stock_id"])->where('trade_date', $data["trade_date"])->exists();
+                        if ($exists == false) {
+                            StockWeek::create($data);
+                        } else {
+                            StockWeek::where('stock_id', $data["stock_id"])->where('trade_date', $data["trade_date"])->update($data);
+                        }
+                    }
+                } else {
+                    StockWeek::insert($insertStockWeekData);
+                }
+            } else {
+                foreach ($insertStockWeekData as $data) {
+                    $exists = StockWeek::where('stock_id', $data["stock_id"])->where('trade_date', $data["trade_date"])->exists();
+                    if($exists) {
+                        StockWeek::where('stock_id', $data["stock_id"])->where('trade_date', $data["trade_date"])->update($data);
+                    } else {
+                        StockWeek::create($data);
+                    }
+                }
+            }
+        }
+    }
+
+    function dealWeekData($weekData, &$lastClose) {
+        $weekDataCollect = Collect($weekData);
+        $change = $weekDataCollect->last()->close - $weekData[0]->open;
+        $pct_chg = $change / $weekData[0]->open;
+        $result = [
+            "stock_id" => $weekData[0]->stock_id,
+            "trade_date" => $weekDataCollect->last()->trade_date,
+            "open" => $weekData[0]->open,
+            "high" => $weekDataCollect->max("high"),
+            "low" => $weekDataCollect->max("low"),
+            "close" => $weekDataCollect->last()->close,
+            "pre_close" => $lastClose,
+            "change" => $weekDataCollect->last()->close - $weekData[0]->open,
+            "pct_chg" => $pct_chg,
+            "vol" => $weekDataCollect->sum('vol'),
+            "amount" => $weekDataCollect->sum('amount'),
+            "fq_factor" => $weekDataCollect->last()->fq_factor
+        ];
+        $lastClose = $weekDataCollect->last()->close;
+        return $result;
+    }
+    public function calcMondayAndFriday($dateStr) {
+
+        $d = Carbon::createFromFormat("Ymd", $dateStr);
+        $monday = $d->copy()->subDays($d->dayOfWeekIso - 1);
+        $friday = $d->copy()->addDays(5 - $d->dayOfWeekIso);
+        return [$monday->format("Ymd"), $friday->format("Ymd")];
     }
 }
